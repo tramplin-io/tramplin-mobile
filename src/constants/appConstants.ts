7b2281e4637515c62159ef53e1b81207f6bc2a99
D6V6S <vitya.demichev@gmail.com>
2026-02-25 12:28:23 +0200

    feat: update app constants for Tramplin branding, including new contact email and package name, and add Android app store URL

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -3,18 +3,20 @@ import * as Application from 'expo-application'
 const APP_VERSION: string = Application.nativeApplicationVersion || '1.0.0'
 const BUILD_NUMBER: number = Number.parseInt(Application.nativeBuildVersion || '1', 10) || 1
 
-// const PRIVACY_POLICY_URL: string = 'https://femfast.io/privacypolicy'
-// const TERMS_OF_USE_URL: string = 'https://femfast.io/termsofuse'
+// const PRIVACY_POLICY_URL: string = ''
+// const TERMS_OF_USE_URL: string = ''
 
-// const CONTACT_US_EMAIL: string = 'support@femfast.io'
+// const CONTACT_US_EMAIL: string = 'support@tramplin.io'
 
-// const ITUNES_ITEM_ID: number = 6744979369 // App ID for iOS
-// const ANDROID_PACKAGE_NAME: string = 'com.femfast.app' // Package name for Android
+const ITUNES_ITEM_ID: number = 6 // App ID for iOS
+const ANDROID_PACKAGE_NAME: string = 'com.tramplin' // Package name for Android
 
 // App Store URLs for force update
-// const IOS_APP_STORE_URL: string =
-//   process.env.EXPO_PUBLIC_IOS_APP_STORE_URL ||
-//   `itms-apps://itunes.apple.com/app/id${ITUNES_ITEM_ID}`
+const IOS_APP_STORE_URL: string =
+  process.env.EXPO_PUBLIC_IOS_APP_STORE_URL || `itms-apps://itunes.apple.com/app/id${ITUNES_ITEM_ID}`
+const ANDROID_APP_STORE_URL: string =
+  process.env.EXPO_PUBLIC_ANDROID_APP_STORE_URL ||
+  `https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE_NAME}`
 
 const API_URLS = {
   PROD: process.env.EXPO_PUBLIC_API_URL || 'https://api.tramplin.io/api',
@@ -27,8 +29,9 @@ export {
   // PRIVACY_POLICY_URL,
   // TERMS_OF_USE_URL,
   // CONTACT_US_EMAIL,
-  // ITUNES_ITEM_ID,
-  // ANDROID_PACKAGE_NAME,
-  // IOS_APP_STORE_URL,
+  ITUNES_ITEM_ID,
+  ANDROID_PACKAGE_NAME,
+  IOS_APP_STORE_URL,
+  ANDROID_APP_STORE_URL,
   API_URLS,
 }
