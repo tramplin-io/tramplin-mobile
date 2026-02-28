d76c141f977b44643820b4a64b9e4bb6050da8bf
D6V6S <vitya.demichev@gmail.com>
2026-02-28 17:06:23 +0200

    refactor: simplify Skeleton component by consolidating imports and improving className handling for better styling consistency

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,38 +1,25 @@
-import { cn } from '@/lib/utils';
-import { View } from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-} from 'react-native-reanimated';
-import * as React from 'react';
+import * as React from 'react'
+import { View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated'
 
-const duration = 1000;
+import { cn } from '@/lib/utils'
 
-function Skeleton({
-  className,
-  ...props
-}: React.ComponentProps<typeof View> & React.RefAttributes<View>) {
-  const sv = useSharedValue(1);
+const duration = 1000
+
+function Skeleton({ className, ...props }: React.ComponentProps<typeof View> & React.RefAttributes<View>) {
+  const sv = useSharedValue(1)
 
   React.useEffect(() => {
-    sv.value = withRepeat(withTiming(0.5, { duration }), -1, true);
-  }, []);
+    sv.value = withRepeat(withTiming(0.5, { duration }), -1, true)
+  }, [])
 
   const style = useAnimatedStyle(
     () => ({
       opacity: sv.value,
     }),
-    [sv]
-  );
-  return (
-    <Animated.View
-      style={style}
-      className={cn('bg-secondary dark:bg-muted rounded-md', className)}
-      {...props}
-    />
-  );
+    [sv],
+  )
+  return <Animated.View style={style} className={cn('bg-content-tertiary  rounded-md', className)} {...props} />
 }
 
-export { Skeleton };
+export { Skeleton }
