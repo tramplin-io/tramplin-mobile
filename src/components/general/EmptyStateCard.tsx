1bde74cd816e876d5a042cd425b5c1ae1116eaef
D6V6S <vitya.demichev@gmail.com>
2026-05-01 18:55:02 +0300

    feat: replace empty state video with new tramplin video and refactor EmptyStateCard component for improved layout and functionality

--- src/components/general/EmptyStateCard.tsx
+++ src/components/general/EmptyStateCard.tsx
@@ -1,20 +1,17 @@
-import { StyleSheet, View } from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient'
+import { View } from 'react-native'
 import { useVideoPlayer, VideoView } from 'expo-video'
 
 import { cn } from '@/lib/utils'
 
-import { Text } from '../ui/text'
-
-const emptyStateCardMobileMp4 = require('@/assets/videos/empty_state.mp4')
+const emptyStateCardMobileMp4 = require('@/assets/videos/empty/tramplin_empty state_white_2x1(mobile).mp4')
 
 // <EmptyStateCard text="No unclaimed rewards." subtext="Your rewards will appear here." />
 
 export function EmptyStateCard({
   className,
-  text,
-  subtext,
-}: Readonly<{ className?: string; text?: string; subtext?: string }>) {
+
+  children,
+}: Readonly<{ className?: string; children?: React.ReactNode }>) {
   const player = useVideoPlayer(emptyStateCardMobileMp4, (p) => {
     p.loop = true
     p.muted = true
@@ -23,42 +20,18 @@ export function EmptyStateCard({
   return (
     <View
       className={cn(
-        'mx-4 my-2 h-32 border border-border-quaternary rounded-lg overflow-hidden flex-col items-center justify-center bg-fill-primary z-1',
+        'h-[170px] border border-border-quaternary rounded-lg overflow-hidden flex-col items-center justify-center z-1 bg-fill-secondary',
         className,
       )}
     >
       <VideoView
         player={player}
-        style={StyleSheet.absoluteFillObject}
-        contentFit="cover"
+        style={{ position: 'absolute', top: '-5%', left: 0, right: 0, height: '110%' }}
+        contentFit="fill"
         nativeControls={false}
         pointerEvents="none"
       />
-      <LinearGradient
-        colors={[
-          'rgba(255, 255, 255, 0.00)',
-          'rgba(255, 255, 255, 1)',
-          'rgba(255, 255, 255, 1)',
-          'rgba(255, 255, 255, 0.00)',
-        ]}
-        locations={[0, 0.3, 0.7, 1]}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-        style={StyleSheet.absoluteFillObject}
-      />
-
-      <View className="flex-col items-center justify-center">
-        {text && (
-          <Text variant="body" className="text-content-tertiary">
-            {text}
-          </Text>
-        )}
-        {subtext && (
-          <Text variant="body" className="text-content-tertiary opacity-40">
-            {subtext}
-          </Text>
-        )}
-      </View>
+      {children}
     </View>
   )
 }
